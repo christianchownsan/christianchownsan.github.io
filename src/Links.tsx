a62579438de67fe5559199e35e74695e6bc13f8d
Christian Chown <christian.chown@santander.co.uk>
2023-03-17 09:23:52 +0000

    feat: extra links

--- src/Links.tsx
+++ src/Links.tsx
@@ -8,6 +8,50 @@ export function Links() {
         Link to{" "}
         <a
           target="_blank"
+          href="https://christianchownsan.github.io"
+          className="links__a"
+          rel="noreferrer"
+        >
+          https://christianchownsan.github.io
+        </a>
+      </p>
+      <p className="links__p">
+        Link to{" "}
+        <a
+          target="_blank"
+          href="https://christianchownsan.github.io/"
+          className="links__a"
+          rel="noreferrer"
+        >
+          https://christianchownsan.github.io/
+        </a>
+      </p>
+      <p className="links__p">
+        Link to{" "}
+        <a
+          target="_blank"
+          href="https://christianchownsan.github.io/common"
+          className="links__a"
+          rel="noreferrer"
+        >
+          https://christianchownsan.github.io/common
+        </a>
+      </p>
+      <p className="links__p">
+        Link to{" "}
+        <a
+          target="_blank"
+          href="https://christianchownsan.github.io/index.html"
+          className="links__a"
+          rel="noreferrer"
+        >
+          https://christianchownsan.github.io/index.html
+        </a>
+      </p>
+      <p className="links__p">
+        Link to{" "}
+        <a
+          target="_blank"
           href="https://d3ech8keeyzc99.cloudfront.net"
           className="links__a"
           rel="noreferrer"
